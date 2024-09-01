8980ff71381c4a5e8421df9bd79ce9d7ef66a517
Wyler Zahm <w3h3z3@gmail.com>
2024-09-01 13:45:53 -0600

    minor fix to cors policy

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="Vehicle Tracking System API")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to your frontend's URL
+    allow_origins=["http://127.0.0.1:3000"],  # Adjust this to your frontend's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
